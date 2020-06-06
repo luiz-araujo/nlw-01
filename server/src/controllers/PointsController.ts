be4941b81544661580b4e2eb1bc769385d13c691
Luiz Araújo <luizcaj@yahoo.com.br>
2020-06-06 12:01:33 -0300

    Upload de imagens e validação das rotas

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,24 +17,34 @@ class PointsController {
       .distinct()
       .select("points.*");
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://192.168.0.17:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
-    const id = request.params.id;
-
-    const point = await knex("points").where("id", id).first();
+    const { id } = request.params;
+    const point = await knex("points").select("*").where({ id }).first();
 
     if (!point) {
-      return response.status(400).json({ message: "Point not found." });
+      return response.status(400).json({ message: "Point not found" });
     }
 
     const items = await knex("items")
       .join("point_items", "items.id", "=", "point_items.item_id")
-      .where("point_items.point_id", id)
-      .select("items.title");
+      .where("point_items.point_id", id);
 
-    return response.json({ point, items });
+    const serializedPoint = {
+      ...point,
+      image: `http://192.168.0.17:3333/uploads/${point.image}`,
+    };
+
+    return response.json({ point: serializedPoint, items });
   }
 
   async create(request: Request, response: Response) {
@@ -52,8 +62,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image:
-        "https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -67,14 +76,16 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
-
-    await trx("point_items").insert(pointItems);
+    const pointItems = items
+      .split(",")
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
+    const tmp = await trx("point_items").insert(pointItems);
 
     await trx.commit();
 
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -10,6 +10,7 @@ import "./styles.css";
 
 import axios from "axios";
 import api from "../../services/api";
+import Dropzone from "../../components/Dropzone";
 
 interface Item {
   id: number;
@@ -49,6 +50,7 @@ const CreatePoint = () => {
     0,
     0,
   ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
@@ -133,18 +135,22 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
 
-    await api.post("/points", data);
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
+
+    await api.post("points", data);
 
     alert("Ponto de coleta criado");
 
@@ -167,6 +173,8 @@ const CreatePoint = () => {
           Cadastro do <br />
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile}></Dropzone>
 
         <fieldset>
           <legend>
